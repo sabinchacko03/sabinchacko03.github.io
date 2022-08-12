7601a69976bd5077fec2a80575ccda21234e1ffb
Sabin <sabin@deliveryzone.ae>
2022-08-12 10:25:22 +0400

    Updates

---
+++ precache-manifest.0310bffdcc40e306d55cf12a91e0b39e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0022718e0eb1da473582e78d629c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce58c68f30750a0d6dcb",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "631e582740d0f5ac431b",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "ce58c68f30750a0d6dcb",
+    "url": "/static/js/2.0491ee46.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0491ee46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631e582740d0f5ac431b",
+    "url": "/static/js/main.92d1cd1f.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
