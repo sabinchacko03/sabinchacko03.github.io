a12aeb1144f635e8980a5685976115932fc52f83
Sabin Chacko <sabinchacko03@gmail.com>
2023-12-09 13:41:32 +0400

    ESlint

--- app/page.js
+++ app/page.js
@@ -68,9 +68,9 @@ export default function Home() {
           Experience
         </h2>
         <div className='space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent'>
-          {experiences.map((experience) => {
+          {experiences.map((experience, index) => {
             return (
-              <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+              <div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active" key={index}>
                 <div class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
                   <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
                     <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
