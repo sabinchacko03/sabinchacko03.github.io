09db3972475cc7a8f9bede188e1b3b747c19a326
Sabin Chacko <sabinchacko03@gmail.com>
2023-12-09 09:17:54 +0400

    First

---
+++ app/skills.js
@@ -0,0 +1,46 @@
+export let skills = [
+    {
+        "name": "PHP",
+        "icon": "devicon-php-plain",
+    },
+    {
+        "name": "Laravel",
+        "icon": "devicon-laravel-plain",
+    },
+    {
+        "name": "Tailwind CSS",
+        "icon": "devicon-tailwindcss-plain",
+    },
+    {
+        "name": "React JS",
+        "icon": "devicon-react-original",
+    },
+    {
+        "name": "HTML 5",
+        "icon": "devicon-html5-plain",
+    },
+    {
+        "name": "CSS 3",
+        "icon": "devicon-css3-plain",
+    },
+    {
+        "name": "JavaScript",
+        "icon": "devicon-javascript-plain",
+    },
+    {
+        "name": "Bootstrap",
+        "icon": "devicon-bootstrap-plain",
+    },
+    {
+        "name": "AWS",
+        "icon": "devicon-amazonwebservices-plain",
+    },
+    {
+        "name": "MySql",
+        "icon": "devicon-mysql-plain",
+    },
+    {
+        "name": "GitHub Actions",
+        "icon": "devicon-github-original",
+    }
+]
